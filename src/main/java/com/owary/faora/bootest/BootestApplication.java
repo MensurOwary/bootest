864e81b2d3f781728a636005284b45c64bba4ddc
MensurOwary <mensurowary@gmail.com>
2018-02-12 00:31:54 +0400

    Initial commit

---
+++ src/main/java/com/owary/faora/bootest/BootestApplication.java
@@ -0,0 +1,12 @@
+package com.owary.faora.bootest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootestApplication.class, args);
+	}
+}
---
+++ src/main/java/com/owary/faora/bootest/MyController.java
@@ -0,0 +1,16 @@
+package com.owary.faora.bootest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController {
+
+        @ResponseBody
+        @RequestMapping("/")
+        public String index(){
+                return "I miss";
+        }
+
+}
